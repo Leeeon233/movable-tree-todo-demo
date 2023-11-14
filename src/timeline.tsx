d2f5d4d4b78a831ad54797cb319c02db9f299032
leeeon233 <leeeon233@gmail.com>
2023-11-14 21:14:52 +0800

    fix: min width hidden footer

--- src/timeline.tsx
+++ src/timeline.tsx
@@ -1,32 +1,30 @@
-import  { useRef } from "react";
+import { useRef } from "react";
 import { ITodoModel } from "./interfaces";
 
-
-const Timeline = ({
-  model
-}: {model: ITodoModel}) => {
+const Timeline = ({ model }: { model: ITodoModel }) => {
   const slide = useRef(null);
   const history = model.history;
-  
+
   return (
-    <div className="slidecontainer" style={{padding: "8px"}}>
-      <p>Check out any version</p>
-      <div style={{display:"flex"}}>
-      <input
-        style={{ width: "400px", margin: "0 auto"}}
-        ref={slide}
-        onChange={(e) => {
-          const v = parseInt(e.target.value);
-          model.checkout(v);
-        }}
-        className="slider"
-        type="range"
-        min={0}
-        max={history.length-1}
-        value={model.version}
-      />
+    <div className="slidecontainer" style={{ padding: "8px" }}>
+      <p style={{ fontSize: "20px", textAlign: "center" }}>
+        Check out any version
+      </p>
+      <div style={{ display: "flex" }}>
+        <input
+          style={{ width: "400px", margin: "0 auto" }}
+          ref={slide}
+          onChange={(e) => {
+            const v = parseInt(e.target.value);
+            model.checkout(v);
+          }}
+          className="slider"
+          type="range"
+          min={0}
+          max={history.length - 1}
+          value={model.version}
+        />
       </div>
-      
     </div>
   );
 };
